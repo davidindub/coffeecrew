f729bb323b1c66ad6023793057e1dc5e44f3e82a
David <69420622+davidindub@users.noreply.github.com>
2023-02-02 15:07:31 +0000

    Add basic products list page

--- products/models.py
+++ products/models.py
@@ -48,7 +48,7 @@ class Product(models.Model):
     brand = models.ForeignKey("Brand", null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ManyToManyField(Image, blank=True)
+    image = models.ManyToManyField(Image, null=True, blank=True)
     stock = models.PositiveSmallIntegerField()
     wish_lists = models.ManyToManyField(User, related_name="wish_list", blank=True)
 
