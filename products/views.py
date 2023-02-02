130a3e70801ea9ca2118ac2f091f81fea91a918b
David <69420622+davidindub@users.noreply.github.com>
2023-02-02 16:49:23 +0000

    Add basic product detail page

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.views import generic, View
 from .models import Product, Coffee
 
 
@@ -10,3 +11,28 @@ def all_products(request):
 
     context = {"products": products}
     return render(request, "products/products.html", context)
+
+
+def product_detail(request, product_id):
+    """
+    renders view for an single products full details
+    """
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {"product": product}
+    return render(request, "products/product_detail.html", context)
+
+
+# class ProductsList(generic.ListView):
+#     model = Product
+#     template_name = "products/products.html"
+
+#     def get_queryset(self):
+#         query_set = super().get_queryset()
+
+#         query_set = query_set.filter(visible_to_customers=True)
+
+#         return query_set
+
+#     def get_context_data(self, **kwargs):
+#         return super().get_context_data(**kwargs)
