dafb0bb4280178c9d69d0d9ccafe87e09e4f0f59
David <davidpkelly@me.com>
2023-02-23 17:25:49 +0000

    Move products sort dropdown templates to includes

--- products/views.py
+++ products/views.py
@@ -92,7 +92,10 @@ class ProductsList(generic.ListView):
             else:
                 context["sort_selected"] = "Price (High to Low)"
         elif sort == 'date_added':
-            context["sort_selected"] = "Newest First"
+            if order == 'asc':
+                context["sort_selected"] = "Newest First"
+            else:
+                context["sort_selected"] = "Oldest First"
 
         context["category"] = get_object_or_404(
             Category, name=category) if category else None
@@ -120,6 +123,7 @@ class StaffMemberRequiredMixin(UserPassesTestMixin):
     """
     Check is the user a staff member to restrict access
     """
+
     def test_func(self):
         return self.request.user.is_staff
 
@@ -163,8 +167,27 @@ class ProductCreate(StaffMemberRequiredMixin, generic.edit.CreateView):
 
     def form_valid(self, form):
         response = super().form_valid(form)
-        messages.success(self.request, "Product added successfully! 👍")
+        messages.success(
+            self.request, f"{self.object.name} added successfully! 👍")
         return response
+
+    def get_success_url(self):
+        return reverse_lazy("product_detail",
+                            kwargs={"product_id": self.object.id})
+
+
+class CoffeeCreate(ProductCreate):
+    """
+    View for creating new Coffee Products
+    """
+    model = Coffee
+    form_class = CoffeeForm
+    template_name = "products/forms/product_form.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["new_coffee"] = True
+        return context
 
     def get_success_url(self):
         return reverse_lazy("product_detail",
@@ -203,7 +226,7 @@ class DepartmentCreate(StaffMemberRequiredMixin, generic.edit.CreateView):
         return response
 
     def get_success_url(self):
-        return reverse_lazy("manage_categories")
+        return reverse_lazy("manage_shop")
 
 
 class DepartmentUpdate(StaffMemberRequiredMixin, generic.edit.UpdateView):
@@ -220,7 +243,7 @@ class DepartmentUpdate(StaffMemberRequiredMixin, generic.edit.UpdateView):
         return response
 
     def get_success_url(self):
-        return reverse_lazy("manage_categories")
+        return reverse_lazy("manage_shop")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -234,7 +257,7 @@ class DepartmentDelete(StaffMemberRequiredMixin, generic.DeleteView):
     """
     model = Department
     template_name = "products/forms/department_delete_form.html"
-    success_url = reverse_lazy("manage_categories")
+    success_url = reverse_lazy("manage_shop")
 
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
@@ -250,7 +273,7 @@ class ManageCategories(generic.ListView):
     """
     View for management dash
     """
-    template_name = 'products/staff/manage_categories.html'
+    template_name = 'products/staff/manage_shop.html'
     queryset = Department.objects.all()
     context_object_name = 'departments'
 
@@ -277,7 +300,7 @@ class CategoryCreate(StaffMemberRequiredMixin, generic.edit.CreateView):
         return response
 
     def get_success_url(self):
-        return reverse_lazy("category_list_staff")
+        return reverse_lazy("manage_shop")
 
 
 class CategoryUpdate(StaffMemberRequiredMixin, generic.edit.UpdateView):
@@ -294,7 +317,7 @@ class CategoryUpdate(StaffMemberRequiredMixin, generic.edit.UpdateView):
         return response
 
     def get_success_url(self):
-        return reverse_lazy("manage_categories")
+        return reverse_lazy("manage_shop")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -308,10 +331,116 @@ class CategoryDelete(StaffMemberRequiredMixin, generic.DeleteView):
     """
     model = Category
     template_name = "products/forms/category_delete_form.html"
-    success_url = reverse_lazy("manage_categories")
+    success_url = reverse_lazy("manage_shop")
 
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
         messages.success(
             request, f"{self.object.name} has been deleted successfully!")
         return response
+
+
+class ManageProducts(ProductsList):
+    """
+    renders view for all products, including sorting and searching
+    """
+    model = Product
+    template_name = "products/staff/manage_products.html"
+    context_object_name = "products"
+
+    def get_queryset(self, **kwargs):
+        queryset = super().get_queryset()
+
+        wishlist = self.kwargs.get('wishlist')
+        department = self.kwargs.get('department')
+        category = self.kwargs.get('category')
+        query = self.request.GET.get('search', None)
+
+        sort = self.request.GET.get('sort')
+        order = self.request.GET.get('order')
+
+        if wishlist:
+            wishlist = get_object_or_404(WishList, user=self.request.user)
+            queryset = wishlist.products.all()
+
+        if department:
+            queryset = queryset.filter(
+                category__department__name=department).distinct()
+
+        if category:
+            queryset = queryset.filter(
+                category__name=category).distinct()
+
+        if query:
+            queryset = queryset.filter(name__icontains=query
+                                       ).distinct() | queryset.filter(
+                description__icontains=query).distinct()
+
+        elif query == "":
+            messages.error(self.request,
+                           "You didn't enter any search criteria.")
+
+            queryset = queryset.none()
+
+        if sort == 'name':
+            if order == 'asc':
+                queryset = queryset.order_by('name')
+            else:
+                queryset = queryset.order_by('-name')
+        elif sort == 'price':
+            if order == 'asc':
+                queryset = queryset.order_by('price')
+            else:
+                queryset = queryset.order_by('-price')
+        elif sort == 'date_added':
+            if order == 'asc':
+                queryset = queryset.order_by('date_added')
+            else:
+                queryset = queryset.order_by('-date_added')
+        elif sort == 'stock':
+            if order == 'asc':
+                queryset = queryset.order_by('stock')
+            else:
+                queryset = queryset.order_by('-stock')
+        elif sort == 'lifetime_sales':
+            if order == 'asc':
+                queryset = queryset.order_by('lifetime_sales')
+            else:
+                queryset = queryset.order_by('-lifetime_sales')
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        wishlist = self.kwargs.get('wishlist')
+        department = self.kwargs.get('department')
+        category = self.kwargs.get('category')
+        query = self.request.GET.get('search', None)
+
+        sort = self.request.GET.get('sort')
+        order = self.request.GET.get('order')
+
+        if sort == 'name':
+            if order == 'asc':
+                context["sort_selected"] = "Name (A-Z)"
+            else:
+                context["sort_selected"] = "Name (Z-A)"
+        elif sort == 'price':
+            if order == 'asc':
+                context["sort_selected"] = "Price (Low to High)"
+            else:
+                context["sort_selected"] = "Price (High to Low)"
+        elif sort == 'date_added':
+            if order == 'asc':
+                context["sort_selected"] = "Oldest First"
+            else:
+                context["sort_selected"] = "Newest First"
+
+        context["category"] = get_object_or_404(
+            Category, name=category) if category else None
+
+        context["department"] = get_object_or_404(
+            Department, name=department) if department else None
+
+        return context
