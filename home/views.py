a237793da1e89313dabc174858dccf053f25bc4f
David <davidpkelly@me.com>
2023-02-23 19:19:49 +0000

    Add placeholder template pages

--- home/views.py
+++ home/views.py
@@ -6,3 +6,16 @@ def index(request):
     renders view for the homepage
     """
     return render(request, "home/index.html")
+
+
+def about(request):
+    """
+    renders view for the about page
+    """
+    return render(request, "home/about.html")
+
+def privacy_policy(request):
+    """
+    renders view for the privacy policy page
+    """
+    return render(request, "home/privacy_policy.html")
