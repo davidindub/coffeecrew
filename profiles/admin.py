021ce19e7c605339bad219eb90c487fc1aa7883d
David <davidpkelly@me.com>
2023-03-15 17:45:37 +0000

    Update Admin panel for profiles app

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
-from django.apps import apps
+from django.contrib import admin
+from .models import WishList, Profile, Address
 
-post_models = apps.get_app_config("profiles").get_models()
 
-for model in post_models:
-    try:
-        admin.site.register(model)
-    except admin.sites.AlreadyRegistered:
-        pass
+class WishListAdmin(admin.ModelAdmin):
+    """
+    Define properties to be shown in Django admin list
+    """
+    list_display = ("user", "num_products")
+    readonly_fields = ("user", "num_products")
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    """
+    Define properties to be shown in Django admin list
+    """
+    list_display = ("user", "shipping_address", "billing_address")
+    readonly_fields = ("user", "shipping_address", "billing_address")
+
+
+class AddressAdmin(admin.ModelAdmin):
+    """
+    Define properties to be shown in Django admin list
+    """
+    list_display = ("profile", "address_line_1")
+
+
+admin.site.register(WishList, WishListAdmin)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Address, AddressAdmin)
